109b6e1b001595e320503c5bb6bbf42984c47f27
sharshar-sb <77451674+sharshar-sb@users.noreply.github.com>
2022-04-22 04:59:50 -1000

    Fixes for R4 data issues (#61)

--- ab2d-filters/src/main/java/gov/cms/ab2d/filter/EOBLoadUtilities.java
+++ ab2d-filters/src/main/java/gov/cms/ab2d/filter/EOBLoadUtilities.java
@@ -36,6 +36,24 @@ public class EOBLoadUtilities {
     }
 
     /**
+     * Parse and return the ExplanationOfBenefit file from a JOSN file in the classpath
+     * @param fileInClassPath - the file name and path in the classpath containing the
+     *             Explanation of Benefit data retrieved from Blue
+     * @return the ExplanationOfBenefit object
+     */
+    public static org.hl7.fhir.r4.model.ExplanationOfBenefit getR4EOBFromFileInClassPath(String fileInClassPath) {
+        if (StringUtils.isBlank(fileInClassPath)) {
+            return null;
+        }
+        try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileInClassPath)) {
+            return FhirContext.forR4().newJsonParser().parseResource(org.hl7.fhir.r4.model.ExplanationOfBenefit.class, inputStream);
+        } catch (Exception ex) {
+            log.error("Unable to open the file", ex);
+            return null;
+        }
+    }
+
+    /**
      * Retrieve the Explanation of Benefit object data from a Reader object
      * @param reader - the reader
      * @return the Explanation of Benefit object
