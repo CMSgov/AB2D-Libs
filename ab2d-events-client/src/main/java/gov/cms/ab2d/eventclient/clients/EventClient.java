4a0ccda167f0fe2a0e82bffba6aa5fe7e5a0f1cf
sharshar-sb <laila.sharshar@semanticbits.com>
2022-07-01 09:59:39 -1000

    Fix checkstyle issues. Fix issue with interdependencies between libraries when the new library hasn't been built yet.

--- ab2d-events-client/src/main/java/gov/cms/ab2d/eventclient/clients/EventClient.java
+++ ab2d-events-client/src/main/java/gov/cms/ab2d/eventclient/clients/EventClient.java
@@ -1,6 +1,5 @@
 package gov.cms.ab2d.eventclient.clients;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import gov.cms.ab2d.eventclient.events.LoggableEvent;
 
 public interface EventClient {
