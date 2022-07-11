5a08b02b6baaddeb8f6770ceb05ebdb9d49a5f2e
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-07-11 14:23:39 -0700

    Increase unit testing

--- ab2d-events-client/src/test/java/gov/cms/ab2d/eventclient/config/SendSqsEventTest.java
+++ ab2d-events-client/src/test/java/gov/cms/ab2d/eventclient/config/SendSqsEventTest.java
@@ -1,6 +1,7 @@
 package gov.cms.ab2d.eventclient.config;
 
 import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.GetQueueUrlResult;
 import com.amazonaws.services.sqs.model.Message;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -10,11 +11,15 @@ import gov.cms.ab2d.eventclient.events.ApiResponseEvent;
 import gov.cms.ab2d.eventclient.events.LoggableEvent;
 import gov.cms.ab2d.eventclient.clients.SQSEventClient;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.http.HttpStatus;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -23,9 +28,13 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import static gov.cms.ab2d.eventclient.clients.SQSConfig.EVENTS_QUEUE;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(properties = { "spring.liquibase.enabled=false"})
+@ExtendWith(OutputCaptureExtension.class)
 @Testcontainers
 public class SendSqsEventTest {
 
@@ -47,7 +56,7 @@ public class SendSqsEventTest {
     }
 
     @Test
-    void testSendMessages() throws JsonProcessingException {
+    void testSendMessages() {
         AmazonSQS amazonSQSSpy = Mockito.spy(amazonSQS);
         SQSEventClient sqsEventClient = new SQSEventClient(amazonSQSSpy, mapper, true);
 
@@ -66,5 +75,34 @@ public class SendSqsEventTest {
 
         assertTrue(message1.get(0).getBody().contains(mapper.writeValueAsString(sentApiRequestEvent)));
         assertTrue(message2.get(0).getBody().contains(mapper.writeValueAsString(sentApiResponseEvent)));
+    }
+
+    @Test
+    void testFailedMapping(CapturedOutput output) {
+        AmazonSQS amazonSQSMock = Mockito.mock(AmazonSQS.class);
+        GetQueueUrlResult queueURL = Mockito.mock(GetQueueUrlResult.class);
+
+        when(amazonSQSMock.getQueueUrl(anyString())).thenReturn(queueURL);
+        when(queueURL.getQueueUrl()).thenReturn("localhost:4321");
+        when(amazonSQSMock.sendMessage(any(SendMessageRequest.class))).thenThrow(new UnsupportedOperationException("I am a failure"));
+        SQSEventClient sqsEventClient = new SQSEventClient(amazonSQSMock, mapper, true);
+
+        ApiRequestEvent sentApiRequestEvent = new ApiRequestEvent("organization", "jobId", "url", "ipAddress", "token", "requestId");
+
+        sqsEventClient.sendLogs(sentApiRequestEvent);
+        Assertions.assertTrue(output.getOut().contains("I am a failure"));
+    }
+
+    @Test
+    void testSendMessagesWhenDisabled() {
+        AmazonSQS amazonSQSSpy = Mockito.spy(amazonSQS);
+        SQSEventClient sqsEventClient = new SQSEventClient(amazonSQSSpy, mapper, false);
+
+        final ArgumentCaptor<LoggableEvent> captor = ArgumentCaptor.forClass(LoggableEvent.class);
+        ApiRequestEvent sentApiRequestEvent = new ApiRequestEvent("organization", "jobId", "url", "ipAddress", "token", "requestId");
+
+        sqsEventClient.sendLogs(sentApiRequestEvent);
+
+        Mockito.verify(amazonSQSSpy, never()).sendMessage(any(SendMessageRequest.class));
     }
 }
