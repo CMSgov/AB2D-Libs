17d98f3f5b6e0a16c58941486730dc1f66ac803d
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-12-02 11:34:52 -0800

    add client and rename

--- ab2d-contracts-client/src/main/java/gov/cms/ab2d/contracts/Feign/ContractFeignClient.java
+++ ab2d-contracts-client/src/main/java/gov/cms/ab2d/contracts/Feign/ContractFeignClient.java
@@ -1,8 +1,7 @@
 package gov.cms.ab2d.contracts.Feign;
 
 import gov.cms.ab2d.contracts.model.ContractAPI;
+import org.springframework.cloud.netflix.feign.FeignClient;
 
 @FeignClient(value = "contract")
-public interface ContractFeign extends ContractAPI {
-
-}
+public interface ContractFeignClient extends ContractAPI { }
