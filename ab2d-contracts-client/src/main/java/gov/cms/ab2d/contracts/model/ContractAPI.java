21826b4f2a972a2cbeb76147446283f76cf47882
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-12-08 10:43:00 -0800

    Update ContractAPI.java

--- ab2d-contracts-client/src/main/java/gov/cms/ab2d/contracts/model/ContractAPI.java
+++ ab2d-contracts-client/src/main/java/gov/cms/ab2d/contracts/model/ContractAPI.java
@@ -1,11 +1,9 @@
 package gov.cms.ab2d.contracts.model;
 
-import gov.cms.ab2d.contracts.model.Contract;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
