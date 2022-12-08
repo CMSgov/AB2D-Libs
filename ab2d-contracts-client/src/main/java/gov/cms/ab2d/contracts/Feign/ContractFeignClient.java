62bd7534a990b7e8f850666bbdf07379818376b2
sb-dkniesteadt <92107122+sb-dkniesteadt@users.noreply.github.com>
2022-12-08 09:43:24 -0800

    Add api and remove feign client

--- ab2d-contracts-client/src/main/java/gov/cms/ab2d/contracts/Feign/ContractFeignClient.java
+++
@@ -1,7 +0,0 @@
-package gov.cms.ab2d.contracts.Feign;
-
-import gov.cms.ab2d.contracts.model.ContractAPI;
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient(value = "contract")
-public interface ContractFeignClient extends ContractAPI { }
--- ab2d-contracts-client/src/main/java/gov/cms/ab2d/contracts/model/ContractAPI.java
+++ ab2d-contracts-client/src/main/java/gov/cms/ab2d/contracts/model/ContractAPI.java
@@ -4,17 +4,20 @@ import gov.cms.ab2d.contracts.model.Contract;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ContractAPI {
-    @GetMapping("/contract/all")
-    List<Contract> getAllAttestedContracts();
+    @GetMapping("/contract")
+    List<Contract> getContracts(@RequestParam("contractId") Optional<Long> contractId);
 
-    @PostMapping("/contract")
+    @PutMapping("/contract")
     void updateContract(@RequestBody Contract contract);
 
-    @GetMapping("/contract")
-    Contract getContractByValue(@RequestParam("contractId") Optional<Long> contractId, @RequestParam("contractNumber") Optional<String> contractNumber);
+    @GetMapping("/contract/{contractNumber}")
+    List<Contract> getContractByNumber(@PathVariable("contractNumber") Optional<String> contractNumber);
 }
+
