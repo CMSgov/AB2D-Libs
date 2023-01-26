31375226f8dcd3a32f9bf41bd62bbf1aee372d24
sb-keane <keane.mcgough@semanticbits.com>
2023-01-26 16:03:35 +0000

    added new tests

---
+++ ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/MiscTest.java
@@ -0,0 +1,40 @@
+package gov.cms.ab2d.snsclient.config;
+
+import gov.cms.ab2d.snsclient.messages.CoverageCountDTO;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import static gov.cms.ab2d.snsclient.messages.AB2DServices.AB2D;
+import static gov.cms.ab2d.snsclient.messages.AB2DServices.BFD;
+import static gov.cms.ab2d.snsclient.messages.AB2DServices.HPMS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MiscTest {
+
+    @Test
+    void coverageCountDTO() {
+        Timestamp time = Timestamp.from(Instant.now());
+        CoverageCountDTO coverage = new CoverageCountDTO();
+        coverage.setContractNumber("test");
+        coverage.setService("test");
+        coverage.setYear(1);
+        coverage.setMonth(1);
+        coverage.setCountedAt(time);
+        CoverageCountDTO coverageConst = new CoverageCountDTO("test", "test", 1, 1, 1, time);
+        assertEquals(coverage.getContractNumber(), coverageConst.getContractNumber());
+        assertEquals(coverage.getService(), coverageConst.getService());
+        assertEquals(coverage.getYear(), coverageConst.getYear());
+        assertEquals(coverage.getMonth(), coverageConst.getMonth());
+        assertEquals(coverage.getCountedAt(), coverageConst.getCountedAt());
+    }
+
+    @Test
+    void AB2DServices() {
+        assertEquals("AB2D", AB2D.toString());
+        assertEquals("BFD", BFD.toString());
+        assertEquals("HPMS", HPMS.toString());
+
+    }
+}
--- ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/SendSnsTest.java
+++ ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/SendSnsTest.java
@@ -1,6 +1,7 @@
 package gov.cms.ab2d.snsclient.config;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import gov.cms.ab2d.eventclient.config.Ab2dEnvironment;
 import gov.cms.ab2d.snsclient.clients.SNSClient;
 import gov.cms.ab2d.snsclient.clients.SNSClientImpl;
 import gov.cms.ab2d.snsclient.clients.SNSConfig;
@@ -11,9 +12,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest//(classes = {SNSClient.class, SNSConfig.class, SNSClientImpl.class})
 @Testcontainers
-public class SendSnsEventTest {
+public class SendSnsTest {
 
     @Container
     private static final AB2DLocalstackContainer LOCALSTACK_CONTAINER = new AB2DLocalstackContainer();
@@ -21,9 +24,23 @@ public class SendSnsEventTest {
     @Autowired
     private SNSClient snsClient;
 
+    @Autowired
+    private Ab2dEnvironment environment;
     @Test
-    void testSendSns() throws JsonProcessingException {
-        snsClient.sendMessage( Topics.COVERAGE_COUNTS.getValue(), "test");
+    void testSendSns() {
+        assertDoesNotThrow(() -> {
+            snsClient.sendMessage(Topics.COVERAGE_COUNTS.getValue(), "test");
+        });
+    }
+
+    @Test
+    void testNoUrl() {
+        System.clearProperty("cloud.aws.end-point.uri");
+        SNSConfig config = new SNSConfig();
+        SNSClientImpl client = new SNSClientImpl(config.amazonSNS(), environment);
+        assertDoesNotThrow(() -> {
+            client.sendMessage(Topics.COVERAGE_COUNTS.getValue(), "test");
+        });
     }
 
 
