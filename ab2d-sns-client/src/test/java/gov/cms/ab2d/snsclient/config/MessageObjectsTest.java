5a18b17074b8c7fde87d0b51f4c148032d8e2c99
sb-keane <keane.mcgough@semanticbits.com>
2023-01-26 16:47:28 +0000

    sonar fixes

--- ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/MessageObjectsTest.java
+++ ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/MessageObjectsTest.java
@@ -11,7 +11,7 @@ import static gov.cms.ab2d.snsclient.messages.AB2DServices.BFD;
 import static gov.cms.ab2d.snsclient.messages.AB2DServices.HPMS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MiscTest {
+class MessageObjectsTest {
 
     @Test
     void coverageCountDTO() {
@@ -20,11 +20,13 @@ public class MiscTest {
         coverage.setContractNumber("test");
         coverage.setService("test");
         coverage.setYear(1);
+        coverage.setCount(1);
         coverage.setMonth(1);
         coverage.setCountedAt(time);
         CoverageCountDTO coverageConst = new CoverageCountDTO("test", "test", 1, 1, 1, time);
         assertEquals(coverage.getContractNumber(), coverageConst.getContractNumber());
         assertEquals(coverage.getService(), coverageConst.getService());
+        assertEquals(coverage.getCount(), coverageConst.getCount());
         assertEquals(coverage.getYear(), coverageConst.getYear());
         assertEquals(coverage.getMonth(), coverageConst.getMonth());
         assertEquals(coverage.getCountedAt(), coverageConst.getCountedAt());
