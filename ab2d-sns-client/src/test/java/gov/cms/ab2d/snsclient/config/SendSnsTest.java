5b66f2a063e4645b7b3cc71097281d39f03ca237
sb-keane <keane.mcgough@semanticbits.com>
2023-01-26 16:38:43 +0000

    added lib to mock static methods, fixed sns default path test

--- ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/SendSnsTest.java
+++ ab2d-sns-client/src/test/java/gov/cms/ab2d/snsclient/config/SendSnsTest.java
@@ -1,12 +1,15 @@
 package gov.cms.ab2d.snsclient.config;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import gov.cms.ab2d.eventclient.config.Ab2dEnvironment;
 import gov.cms.ab2d.snsclient.clients.SNSClient;
 import gov.cms.ab2d.snsclient.clients.SNSClientImpl;
 import gov.cms.ab2d.snsclient.clients.SNSConfig;
 import gov.cms.ab2d.snsclient.messages.Topics;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.junit.jupiter.Container;
@@ -25,7 +28,11 @@ public class SendSnsTest {
     private SNSClient snsClient;
 
     @Autowired
+    private SNSConfig config;
+
+    @Autowired
     private Ab2dEnvironment environment;
+
     @Test
     void testSendSns() {
         assertDoesNotThrow(() -> {
@@ -35,11 +42,19 @@ public class SendSnsTest {
 
     @Test
     void testNoUrl() {
+        AmazonSNSClient sns = config.amazonSNS();
         System.clearProperty("cloud.aws.end-point.uri");
-        SNSConfig config = new SNSConfig();
-        SNSClientImpl client = new SNSClientImpl(config.amazonSNS(), environment);
         assertDoesNotThrow(() -> {
-            client.sendMessage(Topics.COVERAGE_COUNTS.getValue(), "test");
+            try (MockedStatic<AmazonSNSClientBuilder> utilities = Mockito.mockStatic(AmazonSNSClientBuilder.class)) {
+                AmazonSNSClientBuilder builder = Mockito.mock( AmazonSNSClientBuilder.class);
+                utilities.when(AmazonSNSClientBuilder::standard)
+                        .thenReturn(builder);
+                Mockito.when(builder.build())
+                        .thenReturn(sns);
+                SNSConfig snsConfig = new SNSConfig();
+                SNSClientImpl client = new SNSClientImpl(snsConfig.amazonSNS(), environment);
+                client.sendMessage(Topics.COVERAGE_COUNTS.getValue(), "test");
+            }
         });
     }
 
