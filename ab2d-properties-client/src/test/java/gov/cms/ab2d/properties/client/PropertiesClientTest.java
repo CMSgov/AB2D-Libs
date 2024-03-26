e9245e93516fd27263452cf8990401efcf9401f6
Rwolfe-Nava <richardwolfe@navapbc.com>
2024-03-26 11:42:03 -0400

    remove public modifier in PropertiesClientTest

--- ab2d-properties-client/src/test/java/gov/cms/ab2d/properties/client/PropertiesClientTest.java
+++ ab2d-properties-client/src/test/java/gov/cms/ab2d/properties/client/PropertiesClientTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class PropertiesClientTest {
+class PropertiesClientTest {
     @Disabled
     @Test
     void testGettingProperties() {
