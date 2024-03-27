0bed92ac084fad540569e644f8420ef3bd44a5c6
Rwolfe-Nava <richardwolfe@navapbc.com>
2024-03-27 10:45:51 -0400

    fix typo in JobSummaryEvent

--- ab2d-events-client/src/main/java/gov/cms/ab2d/eventclient/events/JobSummaryEvent.java
+++ ab2d-events-client/src/main/java/gov/cms/ab2d/eventclient/events/JobSummaryEvent.java
@@ -26,9 +26,9 @@ public class JobSummaryEvent extends LoggableEvent {
 
     public JobSummaryEvent() {
         // Adding a nested comment here to satisfy sonarqube code smells.
-        // This method is an intentionally blank for the following reason:
+        // This method is intentionally blank for the following reason:
         
-        // This is a constructor for a subtype of loggableEvent. 
+        // This is a constructor for a subtype of LoggableEvent. 
         // We construct this type while overriding asMessage to differentiate functionality.
     }
 
