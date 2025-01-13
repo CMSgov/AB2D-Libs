a88824ea4369da6320557b65cc8edb727c5d6e05
Ben H <133617495+bennavapbc@users.noreply.github.com>
2025-01-13 16:45:59 -0700

    Add new FileOutputTypes for file compression; bump aggregator version (#420)

--- ab2d-aggregator/src/test/java/gov/cms/ab2d/aggregator/FileOutputTypeTest.java
+++ ab2d-aggregator/src/test/java/gov/cms/ab2d/aggregator/FileOutputTypeTest.java
@@ -3,7 +3,9 @@ package gov.cms.ab2d.aggregator;
 import org.junit.jupiter.api.Test;
 
 import static gov.cms.ab2d.aggregator.FileOutputType.DATA;
+import static gov.cms.ab2d.aggregator.FileOutputType.DATA_COMPRESSED;
 import static gov.cms.ab2d.aggregator.FileOutputType.ERROR;
+import static gov.cms.ab2d.aggregator.FileOutputType.ERROR_COMPRESSED;
 import static gov.cms.ab2d.aggregator.FileOutputType.UNKNOWN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,8 +17,12 @@ class FileOutputTypeTest {
     void isErrorFile() {
         String file1 = "abc." + ERROR.getSuffix();
         String file2 = "abc." + DATA.getSuffix();
+        String file3 = "abc." + ERROR_COMPRESSED.getSuffix();
+        String file4 = "abc." + DATA_COMPRESSED.getSuffix();
         assertEquals(ERROR, FileOutputType.getFileType(file1));
         assertEquals(DATA, FileOutputType.getFileType(file2));
+        assertEquals(ERROR_COMPRESSED, FileOutputType.getFileType(file3));
+        assertEquals(DATA_COMPRESSED, FileOutputType.getFileType(file4));
         assertEquals(UNKNOWN, FileOutputType.getFileType("bogus.txt"));
         assertEquals(UNKNOWN, FileOutputType.getFileType((String) null));
     }
@@ -25,7 +31,11 @@ class FileOutputTypeTest {
     void testActualFile() {
         File file1 = new File("abc." + ERROR.getSuffix());
         File file2 = new File("abc." + DATA.getSuffix());
+        File file3 = new File("abc." + ERROR_COMPRESSED.getSuffix());
+        File file4 = new File("abc." + DATA_COMPRESSED.getSuffix());
         assertEquals(ERROR, FileOutputType.getFileType(file1));
         assertEquals(DATA, FileOutputType.getFileType(file2));
+        assertEquals(ERROR_COMPRESSED, FileOutputType.getFileType(file3));
+        assertEquals(DATA_COMPRESSED, FileOutputType.getFileType(file4));
     }
 }
