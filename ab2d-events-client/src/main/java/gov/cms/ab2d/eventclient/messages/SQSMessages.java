eca1b973e00d2419622ab8bea6ae5e2a38f67ccd
smirnovaae <annasmirnova@navapbc.com>
2025-02-03 12:31:14 -0800

    json serializer

--- ab2d-events-client/src/main/java/gov/cms/ab2d/eventclient/messages/SQSMessages.java
+++ ab2d-events-client/src/main/java/gov/cms/ab2d/eventclient/messages/SQSMessages.java
@@ -1,11 +1,13 @@
 package gov.cms.ab2d.eventclient.messages;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
 @Data
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
 public abstract class SQSMessages {
     protected SQSMessages() { }
 }
